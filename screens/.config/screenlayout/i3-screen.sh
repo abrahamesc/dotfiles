b188a3fafd3f4ef1fca932076e765a3777b69464
Abraham Escobedo <abraham94esc@gmail.com>
2024-09-13 15:43:11 -0500

    Added input remapper autoload command to i3 config

--- screens/.config/screenlayout/i3-screen.sh
+++ screens/.config/screenlayout/i3-screen.sh
@@ -6,6 +6,8 @@ if [ $MonitorCount -gt 1 ]
 then 
    sleep 2
    xrandr --output eDP-1 --off  --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-3 --mode 1920x1080 --pos 3840x0 --rotate normal
-   nitrogen --restore >> /dev/null
-fi
+   nitrogen --restore >& /dev/null
+else
+   nitrogen --restore >& /dev/null
 
+fi
