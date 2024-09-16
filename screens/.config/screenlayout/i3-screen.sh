547124a70d738eaf1230986a7c4dfe7d15041666
Abraham Escobedo <abraham94esc@gmail.com>
2024-09-16 18:48:11 -0500

    Fixed script to detect changes

--- screens/.config/screenlayout/i3-screen.sh
+++ screens/.config/screenlayout/i3-screen.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-current_monitors = 1
+CurrentMonitors=1
 
 while true; do
 MonitorCount=$(xrandr | grep " connected " | awk '{print $1}' | wc -l)
 
-if [ $MonitorCount != $current_monitors]
-   if [ $MonitorCount -gt 1 ]
-   then 
-      sleep 2
-      xrandr --output eDP-1 --off  --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-3 --mode 1920x1080 --pos 3840x0 --rotate normal
-      nitrogen --restore >& /dev/null
-   else
-      nitrogen --restore >& /dev/null
 
-   fi
+if [ $MonitorCount != $CurrentMonitors ] && [ $MonitorCount -gt 1 ]
+then
+   xrandr --output eDP-1 --off  --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-3 --mode 1920x1080 --pos 3840x0 --rotate normal
+   nitrogen --restore >& /dev/null
+   CurrentMonitors=$MonitorCount
+else
+   nitrogen --restore >& /dev/null
+
 fi
 sleep 5
 done
-
--- screens/.config/screenlayout/i3-screen_new.sh
+++ screens/.config/screenlayout/i3-screen_new.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-state=0
+CurrentMonitors=1
 
 while true; do
 MonitorCount=$(xrandr | grep " connected " | awk '{print $1}' | wc -l)
 
-if [ $MonitorCount -gt 1 ]
-then 
-   sleep 2
+
+if [ $MonitorCount != $CurrentMonitors ] && [ $MonitorCount -gt 1 ]
+then
    xrandr --output eDP-1 --off  --output DP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-3 --mode 1920x1080 --pos 3840x0 --rotate normal
    nitrogen --restore >& /dev/null
+   CurrentMonitors=$MonitorCount
 else
    nitrogen --restore >& /dev/null
 
