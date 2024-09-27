9a0ae105c8bdf90c6ee08f820c15534e036f443e
Abraham Escobedo <abraham94esc@gmail.com>
2024-09-27 16:38:49 -0500

    Added emoji selector

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -16,7 +16,7 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>!', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   local live_grep = require('telescope.builtin').live_grep
--- nvim/.config/nvim/lua/abe/lazy.lua
+++ nvim/.config/nvim/lua/abe/lazy.lua
@@ -26,6 +26,9 @@ require("lazy").setup({
         },
       },
     {
+        "ziontee113/icon-picker.nvim",
+    },
+    {
       'MeanderingProgrammer/render-markdown.nvim',
       opts = {},
       --dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
